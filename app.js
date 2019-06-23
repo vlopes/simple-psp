3a9f5421a15f69c8ba264accf4a25c3566c5e7bd
Vinicius Domingues <vinicius.ld@outlook.com>
2019-06-23 16:28:53 -0300

    Created the listing routes and treated the value input

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ app.get('/', function (req, res) {
     res.send('teste')
 })
 
-
 app.post('/transaction', function (req, res) {
+    let value = parseFloat(req.body.value)
+    let transactionValue = Math.floor(value * 100)
+
     Transaction.create({
-        value: req.body.value,
+        value: transactionValue,
         description: req.body.description,
         paymentMethod: req.body.payment_method,
         cardNumber: req.body.card_number,
@@ -34,21 +36,39 @@ app.post('/transaction', function (req, res) {
             paymentDate.setDate(paymentDate.getDate() + 30)
         }
 
-        let value = transaction.value * tax
+        let payableValue = transaction.value * tax
+        payableValue = Math.floor(payableValue)
 
         return Payable.create({
             transactionId: transaction.id,
-            value: value,
+            value: payableValue,
             status: status,
             paymentDate: paymentDate
         })
+    }).then(() => {
+        res.status(201).send(
+            { message: 'The transaction was successfuly created!' }
+        )
     })
-        .then(() => {
-            res.status(201).send(
-                { message: 'The transaction was successfuly created!' }
-            )
-        })
 });
+
+app.get('/transaction', function (req, res) {
+    Transaction.findAll().then(
+        (transactions) => res.status(200).send(transactions)
+    )
+})
+
+app.get('/payables/available_funds', function (req, res) {
+    Payable.sum('value', { where: { status: 'paid' } }).then((sum) => {
+        res.status(200).send({ message: 'Available funds: ' + sum.toFixed(2) })
+    })
+})
+
+app.get('/payables/waiting_funds', function (req, res) {
+    Payable.sum('value', { where: { status: 'waiting_funds' } }).then((sum) => {
+        res.status(200).send({ message: 'Waiting funds: ' + sum.toFixed(2) })
+    })
+})
 
 app.listen(3000, function () {
     console.log('Server running on port 3000!');
