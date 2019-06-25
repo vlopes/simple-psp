175a904db16c121ceb0745a10114fda86ecacdd9
Vinicius Domingues <vinicius.ld@outlook.com>
2019-06-25 08:39:20 -0300

    Started to implement swagger to explain the end points

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let express = require('express');
-let transactionRoutes = require('./routes/transaction')
-let payableRoutes = require('./routes/payable')
+let transactionRoutes = require('./routes/transaction');
+let payableRoutes = require('./routes/payable');
 const bodyParser = require('body-parser');
-const errorHandler = require('./controllers/error-handler')
+const errorHandler = require('./controllers/error-handler');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJson = require('./swagger.json')
 
 let app = express();
 app.use(bodyParser.json());
@@ -11,6 +13,8 @@ app.use('/transaction', transactionRoutes);
 app.use('/payable', payableRoutes);
 
 app.use(errorHandler);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.listen(3000, function () {
     console.log('Server running on port 3000!');
--- validators/create-transaction.js
+++ validators/create-transaction.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const schema = Joi.object({
     value: Joi.number().positive().precision(2).required(),
     description: Joi.string().required(),
-    paymentMethod: Joi.string().required(),
+    paymentMethod: Joi.string().valid('credit_card', 'debit_card').required(),
     cardNumber: Joi.string().creditCard().required(),
     ownerName: Joi.string().required(),
     cvv: Joi.string().regex(/^\d{3}$/).required()
